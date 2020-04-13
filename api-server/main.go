0529afa36cd6134f4a42030ee07f464b6e5db58e
近藤大翔 <taisho90@icloud.com>
2020-04-14 08:04:11 +0900

    ユーザー作成API実装

--- api-server/main.go
+++ api-server/main.go
@@ -5,26 +5,55 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"math/rand"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
+// runeってなんやねん
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randSeq(n int) string {
+    b := make([]rune, n)
+    for i := range b {
+        b[i] = letters[rand.Intn(len(letters))]
+    }
+    return string(b)
+}
+
 func main() {
-	cnn, err := sql.Open("mysql", "docker:docker@tcp(db:3306)/game_db")
+	db, err := sql.Open("mysql", "docker:docker@tcp(db:3306)/game_db")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	id := 1
-	var name string
+	http.HandleFunc("/user/create", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "POST" {
+			fmt.Fprintf(w, "Sorry, only Post methods are supported.")
+		}
+		
+		name := r.FormValue("name")
+		token := randSeq(32)
+		// ToDo すでに登録されている名前かどうかを判断する
+		rows, err := db.Query("SELECT name FROM user WHERE name = ?;", name)
 
-	if err := cnn.QueryRow("SELECT name FROM test_tb WHERE id = ? LIMIT 1", id).Scan(&name); err != nil {
-		log.Fatal(err)
-	}
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, name)
+		if rows.Next() {
+			fmt.Fprintf(w, "この名前はすでに登録されています\n")	
+		}else {
+			result, err := db.Exec("INSERT user (name, token, gacha_times) values (?, ?, 0)", name, token)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fmt.Fprintf(w, "Name = %s\n", name)
+		}
 	})
+
+	fmt.Print("start server")
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
