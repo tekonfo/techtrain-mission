11ad6748ca48a1da576275b83ddf5983e986b62d
近藤大翔 <taisho90@icloud.com>
2020-04-15 07:59:46 +0900

    /user機能実装

--- api-server/main.go
+++ api-server/main.go
@@ -27,11 +27,56 @@ type CreateUser struct {
 	Token string `json:"token"`
 }
 
+type GetUser struct {
+	Name string `json:"name"`
+}
+
 func main() {
 	db, err := sql.Open("mysql", "docker:docker@tcp(db:3306)/game_db")
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	http.HandleFunc("/user", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		if r.Method != "GET" {
+			fmt.Fprintf(w, "Sorry, only Get methods are supported")
+			return
+		}
+
+		xtoken, ok := r.URL.Query()["x-token"]
+		if !ok || len(xtoken[0]) < 1 {
+			log.Println("Url param 'key' is  missing")
+		}
+
+		token := xtoken[0]
+
+		// 検索
+		row := db.QueryRow("SELECT name FROM user where token = ?;", token)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var name string
+		err := row.Scan(&name)
+
+		if err != nil {
+			fmt.Fprintf(w, "このtokenを持つユーザーは存在しません\n")
+			return
+		}
+
+		createuser := GetUser{
+			Name: name,
+		}
+
+		b, err := json.Marshal(createuser)
+		if err != nil {
+			fmt.Println("error:", err)
+		}
+		
+		w.Write(b)
+	})
 
 	http.HandleFunc("/user/create", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
